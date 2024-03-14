95f0c8447386d4b34149065969a470c77284570a
Paurikova <miska.paurikova@gmail.com>
2024-03-14 12:32:20 +0000

    compilation done

--- library/include/types.h
+++ library/include/types.h
@@ -1,12 +1,13 @@
-const SimTime_t SIMULATION_TIME_STEP = 1;
-const SimTime_t SIMULATION_TIME_OFFSET = 0;
-
 using AgentId_t = int; // type for agent ID
 using ServiceId_t = int; // type for service ID
 using SimTime_t = float; // type for simulation time
 using ExecFunct_t = std::function<void(int)>; // type for function callback
 
+namespace {
+    const SimTime_t SIMULATION_TIME_OFFSET = 0;
+
 // Static counter for the next agent ID
-static int nextAgentId = 1;
+    int nextAgentId = 1;
 // Static counter for the next message ID
-static int nextMessageId = 1;
+    int nextMessageId = 1;
+}
